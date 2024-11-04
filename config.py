f725a268593cb67aa26195698b044a19bf78b722
Alex Foxleigh <alex@alexfoxleigh.com>
2024-11-04 09:14:15 +0000

    Realised that I'd rather maintain a react blog than a python one so the python code has been archived

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-class Config:
-    SANITY_PROJECT_ID = os.getenv('SANITY_PROJECT_ID')
-    SANITY_DATASET = os.getenv('SANITY_DATASET')
--- run.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-
-print("Starting app creation...")  # Debugging line
-app = create_app()
-print("App created successfully")  # Debugging line
-
-if __name__ == '__main__':
-    print("Running the app...")  # Debugging line
-    app.run(debug=True)
