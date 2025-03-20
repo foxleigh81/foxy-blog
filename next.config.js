50db788e1568234d625c67e295521846f5478cef
Alex Foxleigh <alex@alexfoxleigh.com>
2025-03-20 13:21:21 +0000

    Restores the redirects from the old version

--- next.config.js
+++ next.config.js
@@ -6,6 +6,40 @@ const nextConfig = {
   experimental: {
     allowedDevOrigins: ['localhost', '127.0.0.1'],
   },
+  async redirects() {
+    return [
+      {
+        source: "/digital-industry/diary-of-a-new-contractor",
+        destination: "/digital-industry/becoming-a-contractor",
+        permanent: true,
+      },
+      {
+        source: "/technically-minded/five-months-later-a-disgruntled-re-review-of-the-motorola-milestone",
+        destination: "/meta/this-article-displeased-me",
+        permanent: true,
+      },
+      {
+        source: "/technically-minded/android-update-for-milestone-finally-hits-the-uk",
+        destination: "/meta/this-article-displeased-me",
+        permanent: true,
+      },
+      {
+        source: "/technically-minded/adobe-cs5-launch-the-creative-suite-feature-run-down",
+        destination: "/meta/this-article-displeased-me",
+        permanent: true,
+      },
+      {
+        source: "/technically-minded/review-motorola-milestone-uk",
+        destination: "/meta/this-article-displeased-me",
+        permanent: true,
+      },
+      {
+        source: "/technically-minded/head-cloud-justified-rant-technology",
+        destination: "/technically-minded/head-in-the-cloud",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
