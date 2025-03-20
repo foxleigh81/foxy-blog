b5b03a726fa95bf7f5499a043672208304d3b96f
Alex Foxleigh <alex@alexfoxleigh.com>
2025-03-20 22:42:20 +0000

    Adds nextjs robots.txt generator

---
+++ src/app/robots.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://www.foxleigh.me';
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/api/',
+        '/studio/',
+        '/admin/',
+        '/_next/',
+        '/server-sitemap.xml',
+        '/search',
+      ],
+    },
+    sitemap: `${siteUrl}/sitemap.xml`,
+  };
+}
