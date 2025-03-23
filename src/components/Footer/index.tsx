285153dc8f992c095e04fbe6e4165b41a9dffaa3
Alex Foxleigh <alex@alexfoxleigh.com>
2025-03-23 19:34:36 +0000

    Adds ko-fi image button to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,10 @@
 "use client";
 
 import React from 'react';
+import Image from 'next/image';
 import SocialLinks from '@/components/SocialLinks';
 
 const Footer: React.FC = () => {
-  const kofiScript = `
-    <script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script>
-    <script type='text/javascript'>kofiwidget2.init('Support me on Ko-fi', '#d110a4', 'I3I21FRCN');kofiwidget2.draw();</script>
-  `;
 
   return (
     <footer className="py-8 px-4 mt-8 border-t border-gray-200">
@@ -19,17 +16,23 @@ const Footer: React.FC = () => {
 
         <div className="text-center">
           <p className="m-0 mb-2 text-sm">© {new Date().getFullYear()}, Alexander Foxleigh</p>
-          <p className="m-0">
+          <p className="m-0 mb-4">
             <a href="https://www.alexfoxleigh.com" className="text-black no-underline hover:underline text-sm">
               Built and maintained by Alex Foxleigh
             </a>
           </p>
 
-          {/* Ko-fi Button */}
-          <div
-            className="flex justify-center mt-4"
-            dangerouslySetInnerHTML={{ __html: kofiScript }}
-          />
+          <div className="flex justify-center mt-2">
+            <a href='https://ko-fi.com/I3I21FRCN' target='_blank' rel="noopener noreferrer">
+              <Image
+                width={144}
+                height={36}
+                src='https://storage.ko-fi.com/cdn/kofi4.png?v=6'
+                alt='Buy Me a Coffee at ko-fi.com'
+                style={{ width: 'auto', height: 'auto' }}
+              />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
