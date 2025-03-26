32015a2f3115b4225a06e9851474e363be163818
Alex Foxleigh <alex@alexfoxleigh.com>
2025-03-26 17:36:44 +0000

    Minor tweak to link styles in the article body

--- src/components/BlockContent/index.tsx
+++ src/components/BlockContent/index.tsx
@@ -123,7 +123,7 @@ const BlockContent: React.FC<BlockContentProps> = ({ content }) => {
           <Link
             href={value.href}
             rel={rel}
-            className="text-primary underline hover:text-primary/80 transition-colors"
+            className="no-underline text-purple-700 hover:text-purple-800 hover:underline hover:underline-offset-4 transition-colors"
           >
             {children}
           </Link>
