6bddc6d1e51be0f94f5c2ed27850be830faf60f0
Alex Foxleigh <alex@alexfoxleigh.com>
2025-03-26 17:50:51 +0000

    Adds some style tweaks to increase contrast for a11y

--- src/components/LegacyBanner/index.tsx
+++ src/components/LegacyBanner/index.tsx
@@ -12,7 +12,7 @@ export const LegacyBanner: React.FC<Props> = ({ year }) => {
   return (
     <div className="bg-red-100 p-4 mb-4 rounded-lg">
       <div className="container mx-auto">
-        <p className="text-red-600 text-sm font-bold">
+        <p className="text-red-800 text-sm font-bold">
           <strong>NOTE:</strong> This is a legacy post from {year}. Some content may be outdated and
           images may not be available or be low quality.
         </p>
