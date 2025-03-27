d170b2dcfa0bf4b0a57462df8866d01d138dfeb2
Alex Foxleigh <alex@alexfoxleigh.com>
2025-03-27 11:58:24 +0000

    Adds some deferred loading to the GTM script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,23 +57,19 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          crossOrigin="anonymous"
+          id="gtm-init"
+          strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${gtmId}');`,
-          }}
-        />
-        <Script
-          id="gtm-dataLayer"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];`,
+              window.dataLayer = window.dataLayer || [];
+              window.addEventListener('load', function () {
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${gtmId}');
+              });
+            `,
           }}
         />
       </head>
