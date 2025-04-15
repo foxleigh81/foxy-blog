119b815f9f8a8218eefe9fe6b793591d94a46c03
Alex Foxleigh <alex@alexfoxleigh.com>
2025-04-15 21:41:24 +0100

    Pared down the authentication logic as it was causing edge-cases with 1password enabled

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
 import { Database } from '../types/supabase';
 
 // Create a single supabase client for the entire app
-export const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL || '',
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY || ''
+export const supabase = createBrowserClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
